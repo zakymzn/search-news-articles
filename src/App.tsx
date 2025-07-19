a8c3c7477f6785eb92a9b1412bb22347dd3fcc92
zakymzn <mamurzakynurrokhman@gmail.com>
2025-07-19 20:15:14 +0700

    membuat komponen NewsItem

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import logo from './assets/New_York_Times_T_icon.svg';
 import vector from './assets/businessman_read_newspaper.svg';
 import github_icon from './assets/github-mark.svg';
 import linkedin_icon from './assets/InBug-Black.png';
+import NewsItem from './components/NewsItem';
 
 function App() {
   const [query, setQuery] = useState('');
@@ -103,19 +104,7 @@ function App() {
           response != null && response.status == 'OK' ? (
             <ul className='list-none grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
               {response.response.docs.map((article) => (
-                <li key={article._id} className='bg-gray-100 rounded-xl p-4 hover:scale-105 transition-all shadow-lg'>
-                  <a href={article.web_url} target="_blank" rel="noopener noreferrer">
-                    {article.multimedia.default.url ?
-                      <img src={article.multimedia.default.url} alt="thumbnail" className='rounded-lg w-full h-60 object-cover' />
-                      :
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" className='w-full h-60 bg-gray-200 fill-gray-300 rounded-lg'><path d="M0 0h24v24H0z" fill="none" /><path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z" /></svg>
-                    }
-                    <p className='text-xl font-bold py-2 text-justify'>{article.headline.main}</p>
-                    <p className='text-sm font-medium'>{article.byline.original}</p>
-                    <p className='text-xs font-light'>Published {new Date(article.pub_date).toLocaleString('en-US', { dateStyle: 'full', timeStyle: 'short' })}</p>
-                    <p className='text-justify line-clamp-3 mt-2'>{article.abstract}</p>
-                  </a>
-                </li>
+                <NewsItem key={article._id} article={article} />
               ))}
             </ul>
           ) : (
